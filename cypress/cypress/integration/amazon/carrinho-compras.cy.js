90d3c2f8c201552e9468b85ebda844d5311dba2b
Fernando Silva <fernandoimp@outlook.com.br>
2022-11-23 12:37:09 -0300

    adição de fixtures em testes Amazon

--- cypress/cypress/integration/amazon/carrinho-compras.cy.js
+++ cypress/cypress/integration/amazon/carrinho-compras.cy.js
@@ -1,33 +1,39 @@
 describe('Carrinho de compras', () => {
 
   before(() => {
-    const AMAZON_URL = Cypress.env('AMAZON_URL')
-    cy.visit(AMAZON_URL)
+    const AMAZON_URL = Cypress.env('AMAZON_URL');
+    cy.visit(AMAZON_URL);
   })
   
   it('Cenário 1 - Incluir produto no carrinho', () => {
-    cy.get('#twotabsearchtextbox').type('hunter x hunter 35{enter}', {force:true})
-      .get('[data-asin] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image')
-        .first().click()
-      .get('#add-to-cart-button', { timeout: 10000 }).click()
-      .get('#sw-gtc > .a-button-inner > .a-button-text').click()
-      .get('.a-truncate-cut').should('have.text', 'Hunter X Hunter - Vol. 35')
+    cy.fixture('amazonProducts').as('product').then( function () {
+      cy.get('#twotabsearchtextbox').type(this.product.manga + '{enter}', {force:true});
+    
+
+      cy.get('[data-asin] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image')
+          .first().click()
+        .get('#add-to-cart-button', { timeout: 10000 }).click()
+        .get('#sw-gtc > .a-button-inner > .a-button-text').click();
+      cy.get('.a-truncate-cut').should('have.text', this.product.manga);
+    })
   })
 
   it('Cenário 2 - Incluir produto 3 no carrinho e validar preço total', () => {
-    cy.get('#twotabsearchtextbox').type('copo{enter}', {force:true})
-      .get('[data-asin] > .sg-col-inner > .s-widget-container > [data-component-type="s-impression-logger"] > .s-featured-result-item > .s-card-container > .a-spacing-base > .s-product-image-container')
-        .eq(2).click()
+    cy.fixture('amazonProducts').as('product').then( function () {
+      cy.get('#twotabsearchtextbox').type(this.product.copo + '{enter}', {force:true})
+        .get('[data-asin] > .sg-col-inner > .s-widget-container > [data-component-type="s-impression-logger"] > .s-featured-result-item > .s-card-container > .a-spacing-base > .s-product-image-container')
+          .eq(2).click();
+    })
 
     cy.get('#corePrice_feature_div > .a-section > .a-price > .a-offscreen')
         .invoke('text').then((product_price) => {
           cy.get('#add-to-cart-button').click()
-            .get('#sw-gtc > .a-button-inner > .a-button-text').click()
+            .get('#sw-gtc > .a-button-inner > .a-button-text').click();
 
-            .get('#sc-subtotal-amount-buybox > .a-size-medium')
+        cy.get('#sc-subtotal-amount-buybox > .a-size-medium')
           .invoke('text').then((cart_price) => {
-            let total_price = cart_price
-            expect(total_price.replace('R$','').trim()).to.eq(product_price.replace('R$',''))
+            let total_price = cart_price;
+            expect(total_price.replace('R$','').trim()).to.eq(product_price.replace('R$',''));
         })
         })
   })
--- cypress/cypress/integration/open_weather/open-weather.cy.js
+++ cypress/cypress/integration/open_weather/open-weather.cy.js
@@ -17,7 +17,7 @@ describe('Open Weather', () => {
 
   it('Validar status 400 quando latitude for inválida', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather('a',(this.city.embuGuacu.longitude),false)
+      cy.getWeather(this.city.invalidValue, this.city.embuGuacu.longitude, false)
         .then((response) => {
           expect(response.status).to.equal(400);
       })
@@ -26,7 +26,7 @@ describe('Open Weather', () => {
 
   it('Validar status 400 quando longitude for inválida', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather(this.city.embuGuacu.latitude,'a',false)
+      cy.getWeather(this.city.embuGuacu.latitude, this.city.invalidValue, false)
       .then((response) => {
         expect(response.status).to.equal(400);
       })
@@ -35,7 +35,7 @@ describe('Open Weather', () => {
 
   it('Validar status 400 quando latitude for null', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather(null,this.city.embuGuacu.longitude,false)
+      cy.getWeather(null, this.city.embuGuacu.longitude, false)
       .then((response) => {
         expect(response.status).to.equal(400);
       })
@@ -44,7 +44,7 @@ describe('Open Weather', () => {
 
   it('Validar status 400 quando longitude for null', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather(this.city.embuGuacu.latitude,null,false)
+      cy.getWeather(this.city.embuGuacu.latitude, null, false)
       .then((response) => {
         expect(response.status).to.equal(400);
       })
@@ -53,7 +53,7 @@ describe('Open Weather', () => {
 
   it('Validar status 401 quando API Key inválida', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather((this.city.embuGuacu.latitude),(this.city.embuGuacu.longitude),false,'a')
+      cy.getWeather(this.city.embuGuacu.latitude, this.city.embuGuacu.longitude, false, this.city.invalidValue)
       .then((response) => {
         expect(response.status).to.equal(401);
       })
@@ -62,7 +62,7 @@ describe('Open Weather', () => {
 
   it('Validar cidade com nome "Itupeva" com campos em pt_br', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeatherWithLang((this.city.itupeva.latitude),(this.city.itupeva.longitude),false,'pt_br')
+      cy.getWeatherWithLang(this.city.itupeva.latitude, this.city.itupeva.longitude, false, 'pt_br')
       .then((response) => {
         expect(response.body.weather['0'].description).to.equal('céu limpo');
       })
@@ -71,7 +71,7 @@ describe('Open Weather', () => {
 
   it('Verificar cidade com nome "Embu Guaçu"', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather(this.city.embuGuacu.latitude,this.city.embuGuacu.longitude)
+      cy.getWeather(this.city.embuGuacu.latitude, this.city.embuGuacu.longitude)
         .then((response) => {
           expect(response.body.name).to.equal(this.city.embuGuacu.name);
       })
@@ -80,7 +80,7 @@ describe('Open Weather', () => {
 
   it('Verificar atributo "string" no campo "name"', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather(this.city.itupeva.latitude,this.city.itupeva.longitude)
+      cy.getWeather(this.city.itupeva.latitude, this.city.itupeva.longitude)
         .then((response) => {
           // expect(response.body.name).be.a('string');
           Joi.assert(response.body.name, Joi.string());
@@ -90,7 +90,7 @@ describe('Open Weather', () => {
 
   it('Valida schema da resposta', () => {
     cy.fixture('citys').as('city').then( function () {
-      cy.getWeather(this.city.itupeva.latitude,this.city.itupeva.longitude)
+      cy.getWeather(this.city.itupeva.latitude, this.city.itupeva.longitude)
         .then((response) => {
           const data = response.body;
           joiAssert(data, weather_schema);
